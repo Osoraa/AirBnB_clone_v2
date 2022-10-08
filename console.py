7eacdd91128042bdcb21f82e3edde31d19f55392
Osoraa <sochi98.sn@gmail.com>
2022-10-08 13:49:55 +0100

    Initial commit and cleanup

--- console.py
+++ console.py
@@ -204,11 +204,11 @@ class HBNBCommand(cmd.Cmd):
             if args not in classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in models.storage._FileStorage__objects.items():
                 if k.split(".")[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in models.storage._FileStorage__objects.items():
                 print_list.append(str(v))
 
         print(print_list)
